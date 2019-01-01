e49e684f87d2c226a85a45ce253c2f8bf5ca73d2
yafsw <yafswctrlz@gmail.com>
2019-01-01 07:53:12 +0700

    Update SpecialDeals component

--- src/components/organism/SpecialDeals.js
+++ src/components/organism/SpecialDeals.js
@@ -3,6 +3,7 @@ import Styled from 'styled-components';
 import H1 from '../atoms/H1';
 import P from '../atoms/P';
 import Button from '../atoms/Button';
+import Package from '../molecules/Package';
 
 const SpecialDeals = () => {
     return (
@@ -15,6 +16,26 @@ const SpecialDeals = () => {
                 <div>
                     <Button to="/special-deaals">View All Packages</Button>
                 </div>
+            </div>
+            <div className="packages">
+                <Package                
+                    location="Sumatera Barat, Indonesia"
+                    name="Toba Lake "
+                    src="assets/img/dio-hasibuan-9ZNhQZFivZc-unsplash.jpg"
+                    price="1000"
+                />
+                <Package                
+                    location="Sumatera Barat, Indonesia"
+                    name="Toba Lake "
+                    src="assets/img/dio-hasibuan-9ZNhQZFivZc-unsplash.jpg"
+                    price="1000"
+                />
+                <Package                
+                    location="Sumatera Barat, Indonesia"
+                    name="Toba Lake "
+                    src="assets/img/dio-hasibuan-9ZNhQZFivZc-unsplash.jpg"
+                    price="1000"
+                />
             </div>
         </SpecialDealsStyle>
     );
@@ -49,6 +70,17 @@ const SpecialDealsStyle = Styled.section`
                 margin-top: 2.4rem;
             }
         }
+    }
+
+    .packages {
+        display: grid;
+        grid-template-columns : repeat(auto-fill, minmax(38rem, 1fr));
+        grid-auto-rows : 39rem;
+        place-items: center;
+        grid-gap: 2.4rem;
+        margin-top: 2.4rem;
+        width: 100%;
+        max-width: 128rem;
     }
 `;
 
