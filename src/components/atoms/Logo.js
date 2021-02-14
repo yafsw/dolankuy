7120d1b2ec12203257c5a1eb3593a1aed078e492
yafsw <yafswctrlz@gmail.com>
2021-02-14 20:57:29 +0700

    Add Logo component

---
+++ src/components/atoms/Logo.js
@@ -0,0 +1,20 @@
+import React, { memo } from 'react';
+import Styled from 'styled-components';
+import {Link} from 'react-router-dom'
+
+const Logo = props => {
+    return (
+        <LogoStyle to="/">
+            Dolankuy
+        </LogoStyle>
+    );
+};
+
+const LogoStyle = Styled(Link)`
+    font-size: 2.8rem;
+    color: ${props => props.color ? props.color : 'black'};
+    text-decoration: none;
+    cursor: pointer;
+`;
+
+export default memo(Logo);
