8417d705f3cd17f0582de7534c45ec4ebef4c4b5
yafsw <yafswctrlz@gmail.com>
2021-02-14 20:03:15 +0700

    Add packages data

---
+++ src/data/packages.js
@@ -0,0 +1,148 @@
+const packages = [
+    {
+        id: 1,
+        name: 'Toba Lake',
+        location: 'North Sumatra, Indonesia',
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510166.67101133446!2d98.55508868546423!3d2.6107296724068387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031de07a843b6ad%3A0xc018edffa69c0d05!2sDanau%20Toba!5e0!3m2!1sid!2sid!4v1613289735923!5m2!1sid!2sid',
+        offer: 'special-deal',
+        price: '1.000',
+        contet: {
+            activities: 10,
+            places: 10,
+            duration: 1,
+        },
+        image: [
+            'https://images.unsplash.com/photo-1440558929809-1412944a6225?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1487&q=80',
+            'https://images.unsplash.com/photo-1592639298199-7b9d01c1cf29?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            'https://images.unsplash.com/photo-1601058497548-f247dfe349d6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+        ]
+    },
+    {
+        id: 2,
+        name: 'Nusa Penida',
+        location: 'Bali, Indonesia',
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126189.7540571706!2d115.46742853942104!3d-8.74556653500156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd271194d1319d3%3A0x5c3a3706b2197b7b!2sNusa%20Penida!5e0!3m2!1sid!2sid!4v1613292073791!5m2!1sid!2sid',
+        offer: 'popular',
+        price: '2.000',
+        contet: {
+            activities: 17,
+            places: 15, 
+            duration: 2,
+        },
+        image: [
+            'https://images.unsplash.com/photo-1564092566747-4dd00afb59c8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
+            'https://images.unsplash.com/photo-1579340888456-d50f716d20c2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            'https://images.unsplash.com/photo-1577714593386-295e569b4751?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        ]
+    },
+    {
+        id: 3,
+        name: 'Bunaken Marine Park',
+        location: 'North Sulawesi, Indonesia',
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31905.88320688098!2d124.74202376964153!3d1.6140048743028996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32879a3c17d8d75b%3A0xe07329f226330faf!2sBunaken!5e0!3m2!1sid!2sid!4v1613292762196!5m2!1sid!2sid',
+        offer: 'popular',
+        price: '1.250',
+        contet: {
+            activities: 7,
+            places: 5, 
+            duration: 1,
+        },
+        image: [
+            'https://images.unsplash.com/photo-1510637234398-d25c9570a0a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80',
+            'https://images.unsplash.com/photo-1533914772478-15cc30e93990?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+            'https://images.unsplash.com/photo-1510637858650-c3be04731622?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+        ]
+    },
+    {
+        id: 4,
+        name: 'Wakatobi',
+        location: 'Southeast Sulawesi, Indonesia',
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d508197.77779193514!2d123.51306721384177!3d-5.670398957595411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2da132c009d62f91%3A0x27e60d480afc1529!2sKabupaten%20Wakatobi!5e0!3m2!1sid!2sid!4v1613304904531!5m2!1sid!2sid',
+        offer: 'recommendation',
+        price: '1.500',
+        contet: {
+            activities: 10,
+            places: 10, 
+            duration: 1,
+        },
+        image: [
+            'https://images.unsplash.com/photo-1602144586078-7d95c8d7808c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            'https://images.unsplash.com/photo-1602144586093-06c14ac4fe4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+            'https://images.unsplash.com/photo-1602144586067-2a0b51af7afd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        ]
+    },
+    {
+        id: 5,
+        name: 'Raja Ampat Islands',
+        location: 'West Papua, Indonesia',
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2042455.7210086486!2d129.381160578432!3d-1.031848189475417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d5c3eaaccb47097%3A0x7851bd844c4cdf44!2sKabupaten%20Raja%20Ampat!5e0!3m2!1sid!2sid!4v1613305159887!5m2!1sid!2sid',
+        offer: 'popular',
+        price: '3.000',
+        contet: {
+            activities: 15,
+            places: 12, 
+            duration: 2,
+        },
+        image: [
+            'https://images.unsplash.com/photo-1516690561799-46d8f74f9abf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            'https://images.unsplash.com/photo-1570789210967-2cac24afeb00?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            'https://images.unsplash.com/photo-1564946674274-b1a97f8c77c3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
+        ]
+    },
+    {
+        id: 6,
+        name: 'Mount Bromo',
+        location: 'East Java, Indonesia',
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15806.180045990373!2d112.94423597385335!3d-7.942493358014532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd637aaab794a41%3A0xada40d36ecd2a5dd!2sGn.%20Bromo!5e0!3m2!1sid!2sid!4v1613306101892!5m2!1sid!2sid',
+        offer: 'special-deal',
+        price: '1.000',
+        contet: {
+            activities: 10,
+            places: 8, 
+            duration: 1,
+        },
+        image: [
+            'https://images.unsplash.com/photo-1585357214259-f977cc7d73a4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            'https://images.unsplash.com/photo-1551172897-36d59dd6b7a1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            'https://images.unsplash.com/photo-1598934880292-b476dd6ae202?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+        ]
+    },
+    {
+        id: 7,
+        name: 'Komodo Island',
+        location: 'East Nusa Tenggara, Indonesia',
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252484.52097273996!2d119.33220679350636!3d-8.589207036429192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2db4545ba8b2bbbb%3A0x62b9b2c3feba412!2sPulau%20Komodo!5e0!3m2!1sid!2sid!4v1613306724365!5m2!1sid!2sid',
+        offer: 'special-deal',
+        price: '1.000',
+        contet: {
+            activities: 10,
+            places: 10, 
+            duration: 1,
+        },
+        image: [
+            'https://images.unsplash.com/photo-1604560929658-bbc3c2ba6a36?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+            'https://images.unsplash.com/photo-1562578057-3ca1f7815237?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1078&q=80',
+            'https://images.unsplash.com/photo-1571880826835-7ab2b833ad07?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+        ]
+    },
+    {
+        id: 8,
+        name: 'Borobudur temple',
+        location: 'Central Java, Indonesia',
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15818.868155960785!2d110.1947837738225!3d-7.605739911820444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a8cf009a7d697%3A0xdd34334744dc3cb!2sCandi%20Borobudur!5e0!3m2!1sid!2sid!4v1613307172013!5m2!1sid!2sid',
+        offer: 'recommendation',
+        price: '1.100',
+        contet: {
+            activities: 12,
+            places: 10, 
+            duration: 1,
+        },
+        image: [
+            'https://images.unsplash.com/photo-1566559532224-6d65e9fc0f37?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
+            'https://images.unsplash.com/photo-1557544450-e1200c2c2bb7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1045&q=80',
+            'https://images.unsplash.com/photo-1556375413-f6cdc5e17398?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1060&q=80',
+        ]
+    },
+];
+
+export default packages;
