ec54e7d88fe0918dcaa10d4bf14f282f779185de
yafsw <yafswctrlz@gmail.com>
2021-02-17 16:54:34 +0700

    Add Span component

---
+++ src/components/atoms/Span.js
@@ -0,0 +1,19 @@
+import React, { memo } from 'react';
+import Styled from 'styled-components';
+
+const Span = props => {
+    return (
+        <SpanStyle color={props.color} align={props.align}>
+            {props.children}
+        </SpanStyle>
+    );
+};
+
+const SpanStyle = Styled.p`
+    font-size: 1.6rem;
+    color: ${props => props.color ? props.color : 'black'};
+    text-align: ${props => props.align ? props.align : 'left'};
+    line-height: 1.5;
+`;
+
+export default memo(Span);
