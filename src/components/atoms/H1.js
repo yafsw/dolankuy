d8ecfe1707075cd5876fc6067b055f6b67954f6f
yafsw <yafswctrlz@gmail.com>
2021-02-17 16:09:09 +0700

    Update H1 component style

--- src/components/atoms/H1.js
+++ src/components/atoms/H1.js
@@ -3,7 +3,7 @@ import Styled from 'styled-components';
 
 const H1 = props => {
     return (
-        <H1Style white={props.white} align={props.align}>
+        <H1Style color={props.color} align={props.align}>
             {props.children}
         </H1Style>
     );
@@ -11,7 +11,7 @@ const H1 = props => {
 
 const H1Style = Styled.h1`
     font-size: 4.2rem;
-    color: ${props => props.white ? 'white' : 'black'};
+    color: ${props => props.color ? props.color : 'black'};
     text-align: ${props => props.align ? props.align : 'left'};
     line-height: 1.2;
 `;
