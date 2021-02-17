d12700628fc28ee98c54aa1e371533b47ec2f869
yafsw <yafswctrlz@gmail.com>
2021-02-17 20:03:26 +0700

    Update SignBtn component

--- src/components/molecules/SignBtn.js
+++ src/components/molecules/SignBtn.js
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 import Styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import Span from '../atoms/Span';
+import Button from '../atoms/Button';
 
 const SignBtn = props => {
     return (
@@ -10,7 +11,7 @@ const SignBtn = props => {
                 <Span colot={props.color}>Sign In</Span>
             </Link>
             <Link to="/signup">
-                <Span colot={props.color}>Sign Up</Span>
+                <Button colot={props.color}>Sign Up</Button>
             </Link>
         </SignBtnStyle>
     );
@@ -19,17 +20,15 @@ const SignBtn = props => {
 const SignBtnStyle = Styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: center;
+    justify-content: center;    
     align-items: center;
 
     a {
         text-decoration: none;
 
-        &:nth-child(2) {
+        &:nth-child(2) {    
             margin-left: 1.2rem;
             padding: .6rem 1.8rem;
-            background-color: ${props => props.color};
-            color: ${props => props.color === 'white' ? 'black' : 'white'};
         }
     }
 `;
