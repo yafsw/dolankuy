51d364653a7d11a89e1def7c5f8f928ae53ba8a0
yafsw <yafswctrlz@gmail.com>
2021-02-18 20:36:43 +0700

    Update Bar component

--- src/components/organism/Bar.js
+++ src/components/organism/Bar.js
@@ -2,22 +2,33 @@ import React, { memo } from 'react';
 import Styled from 'styled-components';
 import Menu from '../molecules/Menu';
 import SignBtn from '../molecules/SignBtn';
+import { useApp } from '../../context';
 
 const Bar = () => {
+    const { state } = useApp();
+
     return (
-        <BarStyle>
-            <Menu />
-            <SignBtn />
+        <BarStyle show={state.bar}>
+            <div>
+                <Menu />
+            </div>
+            <div className="sign">
+                <SignBtn />
+            </div>
         </BarStyle>
     );
 };
 
 const BarStyle = Styled.div`
-    display: flex;
+    display: ${props => props.show ? 'flex' : 'none'};
     flex-direction: column;
     justify-coontent: center;
-    align-items: center;
+    align-items: flex-start;
     width: 100%;
+
+    div {
+        margin-top: 1.2rem;
+    }
 `;
 
 export default memo(Bar);
