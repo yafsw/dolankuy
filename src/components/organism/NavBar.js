0531c5d0bce24aa2e45b9c7daec2b37d10c69e01
yafsw <yafswctrlz@gmail.com>
2021-02-18 14:05:57 +0700

    Update NavBar component

--- src/components/organism/NavBar.js
+++ src/components/organism/NavBar.js
@@ -3,13 +3,21 @@ import Styled from 'styled-components';
 import Logo from "../atoms/Logo";
 import Menu from "../molecules/Menu";
 import SignBtn from "../molecules/SignBtn";
+import { IoMenu } from 'react-icons/io5';
 
 const NavBar = () => {
     return (
         <NavBarStyle>
             <Logo />
-            <Menu />
-            <SignBtn />
+            <div className="desktop">
+                <Menu />
+            </div>
+            <div className="desktop">
+                <SignBtn />
+            </div>
+            <div className="mobile">
+                <IoMenu className="menu" />
+            </div>
         </NavBarStyle>
     );
 };
@@ -21,6 +29,31 @@ const NavBarStyle = Styled.nav`
     align-items: center;
     width: 100%;
     max-width: 128rem;
+
+    .desktop {
+        display: flex;
+        justify-content: center;
+        align items: center;
+
+        @media(max-width: 48rem) {
+            display: none;
+        }
+    }
+
+    .mobile {
+        display: none;
+        justify-content: center;
+        align-items: center;
+
+        @media(max-width: 48rem) {
+            display: flex;
+        }
+    }
+
+    .menu {
+        font-size: 4rem;
+        color: black;
+    }
 `;
 
 export default memo(NavBar);
