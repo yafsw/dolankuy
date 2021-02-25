4ed907a3c21099469af48458e97eab218e5e12a7
yafsw <yafswctrlz@gmail.com>
2021-02-25 19:28:44 +0700

    Update Home page' '

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,11 +1,30 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import Header from '../layout/Header';
 import MainHome from '../layout/MainHome';
+import { useApp } from '../context';
 
 const Home = () => {
+    const { state } = useApp();
+    const [scroll, setScroll] = useState('transparent');
+
+    useEffect(() => {
+        window.onscroll = () => {
+            document.body.scrollTop > 40 || document.documentElement.scrollTop > 40
+                ? setScroll('black')
+                : setScroll('transparent')
+        };
+
+        const background = () => {
+            state.bar
+                ? setScroll('black')
+                : setScroll('transparent')
+        };
+        background();
+    }, [state.bar]);
+
     return (
         <>
-            <Header color="white" />
+            <Header color="white" background={scroll} />
             <MainHome />
         </>
     );
