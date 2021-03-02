7782eaced5bff9e266df7b8a0ea2011bb21406b4
yafsw <yafswctrlz@gmail.com>
2021-03-02 13:52:58 +0700

    Update Package component

--- src/components/molecules/Package.js
+++ src/components/molecules/Package.js
@@ -2,21 +2,41 @@ import React, { memo } from 'react';
 import Styled from 'styled-components';
 import Button from '../atoms/Button';
 import P from '../atoms/P';
+import { primary } from '../../config/color';
+import { IoLocationSharp, IoCompass } from 'react-icons/io5';
+import { RiFootprintFill } from 'react-icons/ri';
 
 const Package = props => {
     return (
         <PackageStyle>
-            <small>
+            <div className="text-icon">
+                <IoLocationSharp className="icon"/>
                 <P color="grey">{props.location}</P>
-            </small>
-            <b>
-                <P>{props.name}</P>
-            </b>
+            </div>
+            <div className="name">
+                <b>
+                    <P>{props.name}</P>
+                </b>
+            </div>
             <div className="image">
                 <img src={props.src} alt={props.src} />
             </div>
+            <div className="contents">
+                <div className="text-icon">
+                    <RiFootprintFill className="icon" />
+                    <P color="grey">10 Activities</P>
+                </div>
+                <div className="text-icon">
+                    <IoCompass className="icon" />
+                    <P color="grey">10 Places</P>
+                </div>
+                <div className="text-icon">
+                    <RiFootprintFill />
+                    <P color="grey">1 Week</P>
+                </div>
+            </div>
             <div className="avaibility">
-                <P>{props.price}/Person</P>
+                <P>${props.price}/Person</P>
                 <Button to="/avaibility">View Availabillity</Button>
             </div>
         </PackageStyle>
@@ -31,7 +51,7 @@ const PackageStyle = Styled.div`
     background-color: white;
     box-shadow: 0 1.5rem 4rem rgba(15,48,87,.1);
     border-radius: 1.4rem;
-    padding: 1.2rem;
+    padding: 2.4rem;
 
     .image {
         width: 100%;
@@ -40,7 +60,6 @@ const PackageStyle = Styled.div`
         justify-content: center;
         align-items: center;
         overflow: hidden;
-        margin: 1.2rem 0;
         height: 18rem;
 
         img {
@@ -55,7 +74,31 @@ const PackageStyle = Styled.div`
         justify-content: space-between;
         align-items: center;
         width: 100%;
-        padding-left: 1.2rem;
+    }   
+
+    .text-icon {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        margin-right: 1.2rem;
+    }
+
+    .icon {
+        color: ${primary};
+        font-size: 2rem;
+        margin-right: .6rem
+    }
+
+    .contents {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        margin: 1.2rem 0;
+        width: 100%;
+    }
+
+    .name {
+        margin: 1.2rem 0;
     }
 `;
 
